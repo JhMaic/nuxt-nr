3d3b57c069cf4b5972bfb51d370636127394cb55
JhMaic <owcin.cjh@gmail.com>
2025-04-05 16:35:54 +0900

    fix: 1

--- plugins/nr.ts
+++ plugins/nr.ts
@@ -1,30 +1,30 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook('app:created', async () => {
-    const { BrowserAgent } = await import('@newrelic/browser-agent/loaders/browser-agent')
-    const { JSErrors } = await import('@newrelic/browser-agent/features/jserrors')
-    const { Spa } = await import('@newrelic/browser-agent/features/spa')
+export default defineNuxtPlugin(async (nuxtApp) => {
+  const { BrowserAgent } = await import('@newrelic/browser-agent/loaders/browser-agent')
+  const { JSErrors } = await import('@newrelic/browser-agent/features/jserrors')
+  const { Spa } = await import('@newrelic/browser-agent/features/spa')
 
-    useState('browserAgent', () => new BrowserAgent({
-      init: {
-        distributed_tracing: { enabled: true },
-        privacy: { cookies_enabled: true },
-        ajax: { deny_list: ['bam.nr-data.net'] },
-      },
-      loader_config: {
-        accountID: '6579780',
-        trustKey: '6579780',
-        agentID: '1386259234',
-        licenseKey: 'NRJS-2f3e4c53b27e1407637',
-        applicationID: '1386259234',
-      },
-      info: {
-        beacon: 'bam.nr-data.net',
-        errorBeacon: 'bam.nr-data.net',
-        licenseKey: 'NRJS-2f3e4c53b27e1407637',
-        applicationID: '1386259234',
-        sa: 1,
-      },
-      features: [Spa, JSErrors],
-    }))
+  const nr = new BrowserAgent({
+    init: {
+      distributed_tracing: { enabled: true },
+      privacy: { cookies_enabled: true },
+      ajax: { deny_list: ['bam.nr-data.net'] },
+    },
+    loader_config: {
+      accountID: '6579780',
+      trustKey: '6579780',
+      agentID: '1386259234',
+      licenseKey: 'NRJS-2f3e4c53b27e1407637',
+      applicationID: '1386259234',
+    },
+    info: {
+      beacon: 'bam.nr-data.net',
+      errorBeacon: 'bam.nr-data.net',
+      licenseKey: 'NRJS-2f3e4c53b27e1407637',
+      applicationID: '1386259234',
+      sa: 1,
+    },
+    features: [Spa, JSErrors],
   })
+
+  useState('browserAgent', () => nr)
 })
